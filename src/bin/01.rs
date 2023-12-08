cc59eb8fc6ec797c4cb26747666aa4fb83405e6f
Chris <cmann@overstoneassociates.com>
2023-12-08 21:54:16 +0000

    Initial attempt for D1P2 that doesn't work

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use regex::Regex;
 advent_of_code::solution!(1);
 
 pub fn part_one(input: &str) -> Option<u32> {
@@ -17,7 +20,38 @@ pub fn part_one(input: &str) -> Option<u32> {
 }
 
 pub fn part_two(input: &str) -> Option<u32> {
-    None
+    let digits = Regex::new(r"\d|one|two|three|four|five|six|seven|eight|nine").unwrap();
+    let mut result = 0;
+    let map_numbers = HashMap::from([
+        ("one", 1),
+        ("two", 2),
+        ("three", 3),
+        ("four", 4),
+        ("five", 5),
+        ("six", 6),
+        ("seven", 7),
+        ("eight", 8),
+        ("nine", 9),
+        ("1", 1),
+        ("2", 2),
+        ("3", 3),
+        ("4", 4),
+        ("5", 5),
+        ("6", 6),
+        ("7", 7),
+        ("8", 8),
+        ("9", 9),
+    ]);
+    for line in input.lines() {
+        let mut matches = digits.find_iter(line);
+        let first = map_numbers.get(matches.next().unwrap().as_str()).unwrap();
+        if let Some(last) = matches.last() {
+            result += (first * 10) + map_numbers.get(last.as_str()).unwrap();
+        } else {
+            result += (first * 10) + first;
+        }
+    }
+    Some(result)
 }
 
 #[cfg(test)]
